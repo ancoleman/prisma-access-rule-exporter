40e36eeae584b79059c57c12b0aa7668385c6274
Anton Coleman <acoleman@paloaltonetworks.com>
2023-02-07 17:03:22 -0600

    auth rule example

--- example.py
+++ example.py
@@ -23,3 +23,11 @@ rule_exporter.cleanup_duplicate_rules(folders, decryption_rules)
 rule_exporter.generate_json_file('decryption_rules.json', decryption_rules)
 rule_exporter.generate_csv_rules(folders, decryption_rules, type='decrypt', suffix='rules')
 
+######################
+# Authentication Rules
+######################
+auth_rules = rule_exporter.get_rules(session, folders, rule_type='auth')
+rule_exporter.cleanup_duplicate_rules(folders, auth_rules)
+rule_exporter.generate_json_file('auth_rules.json', auth_rules)
+rule_exporter.generate_csv_rules(folders, auth_rules, type='auth', suffix='rules')
+
